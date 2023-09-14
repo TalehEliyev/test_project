3b844bdd8ca6ff379814bacd551d1dec7e555c09
taleh085 <talehalizada85@gmail.com>
2023-09-14 13:21:50 +0400

    first commit

---
+++ app/src/main/java/com/taleheliyev/homework14/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.taleheliyev.homework14
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.task_15)
+    }
+}
